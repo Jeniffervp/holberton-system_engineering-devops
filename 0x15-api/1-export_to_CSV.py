6d3d26e41d84c6cf2d194ad799f7cf882f78d46f
jeniffervp <737@holbertonschool.com>
2019-09-16 22:19:26 +0000

    [Feature]export to csv file info from the api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" export to csv file """
+import csv
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+
+    to_do = get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                .format(argv[1])).json()
+    usr = get('https://jsonplaceholder.typicode.com/users/{}'
+              .format(argv[1])).json()
+
+    with open('{}.csv'.format(argv[1]), 'w') as my_csv:
+        w_csv = csv.writer(my_csv, quoting=csv.QUOTE_ALL)
+        for a in to_do:
+            data = []
+            data = [a.get('userId'), usr.get('username'),
+                    a.get('completed'), a.get('title')]
+            w_csv.writerow(data)
