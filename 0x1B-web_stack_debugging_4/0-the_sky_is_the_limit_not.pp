21a1f3eaa486d57f533ec089fb73679e54e25073
jeniffervp <737@holbertonschool.com>
2019-10-08 15:46:08 +0000

    [Feature]fix the bug

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+file { '/etc/default/nginx':
+  ensure => 'present',
+}
+
+-> exec { 'replace':
+  command => "sed -i 's/15/4096/g' /etc/default/nginx",
+  path    => ['/usr/bin', 'usr/sbin', '/bin' ],
+}
+
+-> exec { 'restart':
+  command => "sudo service nginx restart",
+  path    => ['/usr/bin','/usr/sbin'],
+}
